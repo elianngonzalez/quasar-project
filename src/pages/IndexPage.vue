f648fbea23c0f1aabeb534abed2621d1e03594f8
elian gonzalez <elianngonzalez7@gmail.com>
2022-08-04 14:02:10 -0300

    IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page>
     <div class="center-items">
-      <img src="https://i.pinimg.com/originals/63/eb/cb/63ebcb2e3ca90717c34470e7407b00c4.jpg">
+      <img src="https://i.pinimg.com/originals/63/eb/cb/63ebcb2e3ca90717c34470e7407b00c4.jpg" width="300" >
       <h3>mi primer app con quasar</h3>
       <p>compilada con capacitor para android</p>
     </div>
